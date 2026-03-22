35e9a344fb25459957e17ced051d6941a0980515
Nathaniel Schaffer <nats@axis>
2026-03-22 13:53:24 -0600

    test case upload

---
+++ cases/percMalAPC3/assets/20250327dLeftSItry1_spin-CmD6mxkO.js
@@ -0,0 +1,1 @@
+const s="/talks/cases/percMalAPC3/assets/20250327dLeftSItry1_spin-Cmg7UMak.mp4";export{s as _};
---
+++ cases/percMalAPC3/assets/20250328postop_CT-DrSkNDLj.js
@@ -0,0 +1,1 @@
+const s="/talks/cases/percMalAPC3/assets/20250328postop_CT-DYDUj5YL.mp4";export{s as _};
